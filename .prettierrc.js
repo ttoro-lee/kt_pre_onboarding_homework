528c16dcf3c80d55d806cd2ba4cae763a97c6fe9
ttoro <dnsanswkd@naver.com>
2023-10-04 11:58:55 +0900

    update : formating rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 //
 
 module.exports = {
-    tabWidth: 2, // tab키 몇 칸 띄울지
+  tabWidth: 2, // tab키 몇 칸 띄울지
   printWidth: 100, // printWidth default 80 => 100 으로 변경
   singleQuote: true, // "" => ''
-  arrowParens: "avoid", // arrow function parameter가 하나일 경우 괄호 생략
+  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
 };
---
+++ index.js
@@ -0,0 +1,5 @@
+if (true) {
+  console.log('hello, world');
+}
+
+var x = 1234;
